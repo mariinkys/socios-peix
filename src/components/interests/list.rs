93e5a48d2966a6e987a287238d5324f4c0236eea
mariinkys <kysgeneral.nrgua@slmail.me>
2025-07-22 09:08:09 +0200

    fix: list components crash on refresh

--- src/components/interests/list.rs
+++ src/components/interests/list.rs
@@ -6,7 +6,7 @@ use crate::{
 
 #[component]
 pub fn InterestsList() -> impl IntoView {
-    let interests = Resource::new(|| (), |_| get_all_interests());
+    let interests = OnceResource::new(get_all_interests());
 
     view! {
         <Transition fallback=move || view! { <PageLoadingComponent/> }>
@@ -17,48 +17,36 @@ pub fn InterestsList() -> impl IntoView {
                     <div class="card-body">
                         <div class="flex gap-1">
                             <h2 class="card-title grow">"Todos los Intereses"</h2>
-                            <button class="btn btn-warning"
-                                on:click=move |_| interests.refetch()
-                            >
-                                "Recargar"
-                            </button>
                             <a class="btn btn-primary" href="/interests/new">"Añadir Interés"</a>
                         </div>
                         <Show
                             when=move || { interests.get().is_some_and(|x| x.is_ok_and(|y| !y.is_empty()))}
                             fallback=|| view! { <p class="text-center">"No hay intereses..."</p> }
                         >
-                            { move || {
-                                interests.get().map(move |x| {
-                                    x.map(move |res| {
-                                        let interests_result = RwSignal::new(res);
-                                        view! {
-                                            <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-200">
-                                                <table class="table">
-                                                    <thead>
-                                                    <tr>
-                                                        <th></th>
-                                                        <th>"Nombre"</th>
-                                                        <th>"Descripción"</th>
-                                                        <th class="text-right">"Editar"</th>
-                                                    </tr>
-                                                    </thead>
-                                                    <For each=move || interests_result.get() key=|i| i.id children=move |i| {
-                                                        view! {
-                                                            <tr>
-                                                                <th>{i.id.unwrap_or_default()}</th>
-                                                                <td>{i.name}</td>
-                                                                <td>{i.description}</td>
-                                                                <td class="text-right"><a class="btn btn-primary" href=format!("/interests/{}", i.id.unwrap_or_default())>"Ver"</a></td>
-                                                            </tr>
-                                                        }
-                                                    }/>
-                                                </table>
-                                            </div>
-                                        }
-                                    })
-                                })
-                            }}
+                            <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-200">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>"Nombre"</th>
+                                        <th>"Descripción"</th>
+                                        <th class="text-right">"Editar"</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                        <For each=move || interests.get_untracked().and_then(|res| res.ok()).unwrap_or_default() key=|i| i.id children=move |i| {
+                                            view! {
+                                                <tr>
+                                                    <th>{i.id.unwrap_or_default()}</th>
+                                                    <td>{i.name}</td>
+                                                    <td>{i.description}</td>
+                                                    <td class="text-right"><a class="btn btn-primary" href=format!("/interests/{}", i.id.unwrap_or_default())>"Ver"</a></td>
+                                                </tr>
+                                            }
+                                        }/>
+                                    </tbody>
+                                </table>
+                            </div>
                         </Show>
                     </div>
                 </div>
--- src/components/members/list.rs
+++ src/components/members/list.rs
@@ -1,10 +1,13 @@
 use leptos::prelude::*;
 
-use crate::{components::page_loading::PageLoadingComponent, core::api::members::get_all_members};
+use crate::{
+    components::page_loading::PageLoadingComponent,
+    core::api::members::{self, get_all_members},
+};
 
 #[component]
 pub fn MembersList() -> impl IntoView {
-    let members = Resource::new(|| (), |_| get_all_members());
+    let members = OnceResource::new(get_all_members());
 
     view! {
         <Transition fallback=move || view! { <PageLoadingComponent/> }>
@@ -15,54 +18,42 @@ pub fn MembersList() -> impl IntoView {
                     <div class="card-body">
                         <div class="flex gap-1">
                             <h2 class="card-title grow">"Todos los Socios"</h2>
-                            <button class="btn btn-warning"
-                                on:click=move |_| members.refetch()
-                            >
-                                "Recargar"
-                            </button>
                             <a class="btn btn-primary" href="/members/new">"Añadir Socio"</a>
                         </div>
                         <Show
                             when=move || { members.get().is_some_and(|x| x.is_ok_and(|y| !y.is_empty()))}
                             fallback=|| view! { <p class="text-center">"No hay socios..."</p> }
                         >
-                            { move || {
-                                members.get().map(move |x| {
-                                    x.map(move |res| {
-                                        let members_result = RwSignal::new(res);
-                                        view! {
-                                            <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-200">
-                                                <table class="table">
-                                                    <thead>
-                                                    <tr>
-                                                        <th></th>
-                                                        <th>"Nombre Completo"</th>
-                                                        <th>"Email"</th>
-                                                        <th>"Fecha de Nacimiento"</th>
-                                                        <th>"Género"</th>
-                                                        <th>"País"</th>
-                                                        <th class="text-right">"Editar"</th>
-                                                    </tr>
-                                                    </thead>
-                                                    <For each=move || members_result.get() key=|m| m.id children=move |m| {
-                                                        view! {
-                                                            <tr>
-                                                                <th>{m.id.unwrap_or_default()}</th>
-                                                                <td>{format!("{} {} {}", m.name, m.surname, m.second_surname)}</td>
-                                                                <td>{m.email}</td>
-                                                                <td>{m.birthdate.map(|x| x.format("%d-%m-%Y").to_string()).unwrap_or_else(|| "N/A".to_string())}</td>
-                                                                <td>{m.gender.to_string()}</td>
-                                                                <td>{m.country.to_string()}</td>
-                                                                <td class="text-right"><a class="btn btn-primary" href=format!("/members/{}", m.id.unwrap_or_default())>"Ver"</a></td>
-                                                            </tr>
-                                                        }
-                                                    }/>
-                                                </table>
-                                            </div>
-                                        }
-                                    })
-                                })
-                            }}
+                            <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-200">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>"Nombre Completo"</th>
+                                        <th>"Email"</th>
+                                        <th>"Fecha de Nacimiento"</th>
+                                        <th>"Género"</th>
+                                        <th>"País"</th>
+                                        <th class="text-right">"Editar"</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                        <For each=move || members.get_untracked().and_then(|res| res.ok()).unwrap_or_default() key=|m| m.id children=move |m| {
+                                            view! {
+                                                <tr>
+                                                    <th>{m.id.unwrap_or_default()}</th>
+                                                    <td>{format!("{} {} {}", m.name, m.surname, m.second_surname)}</td>
+                                                    <td>{m.email}</td>
+                                                    <td>{m.birthdate.map(|x| x.format("%d-%m-%Y").to_string()).unwrap_or_else(|| "N/A".to_string())}</td>
+                                                    <td>{m.gender.to_string()}</td>
+                                                    <td>{m.country.to_string()}</td>
+                                                    <td class="text-right"><a class="btn btn-primary" href=format!("/members/{}", m.id.unwrap_or_default())>"Ver"</a></td>
+                                                </tr>
+                                            }
+                                        }/>
+                                    </tbody>
+                                </table>
+                            </div>
                         </Show>
                     </div>
                 </div>
